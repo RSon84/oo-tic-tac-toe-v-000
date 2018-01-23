2303f823b9f6896e573602381895824d38839ae0
Devin Wilson <craigvin84@yahoo.com>
2018-01-23 23:05:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
 end
 
   def display_board(board)
-    @board =
+    array.new =
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
