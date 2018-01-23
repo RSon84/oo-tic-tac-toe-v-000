3a11fe4c88a2091423844385e945aa893958f2da
Devin Wilson <craigvin84@yahoo.com>
2018-01-23 22:48:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,8 +2,8 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-  def WIN_COMBINATIONS= arrays[" ", " ", " "]
-    @WIN_COMBINATIONS= arrays[
+  def WIN_COMBINATIONS= combos[" ", " ", " "]
+    @WIN_COMBINATIONS= combos[
     [0,1,2],
     [3,4,5],
     [6,7,8],
