72136533cc894bf29ccf19d32316e77bd93d7441
Devin Wilson <craigvin84@yahoo.com>
2018-01-24 03:46:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,16 +64,17 @@ def current_player
 end
 
 def turn
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  current_player = @board[index]
-  if valid_move?(index)
-    move(index, current_player)
-  else
-    turn
-  end
-  display_board
-end
-
+   puts "Please enter 1-9:"
+   input= gets.chomp
+   index= input_to_index(user_input)
+   if valid_move?(user_input)== true
+     move(user_input,current_player)
+     display_board
+   else
+   until valid_move?(user_input)== true
+       puts "Please enter 1-9:"
+       user_input= gets.chomp
+       index = input_to_index(user_input)
+     end
+   end
 end
