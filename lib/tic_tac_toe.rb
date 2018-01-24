7e7bfe9eb0044b7cc7a567bd496d6201bfc68e3a
Devin Wilson <craigvin84@yahoo.com>
2018-01-24 02:00:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,4 +29,12 @@ WIN_COMBINATIONS = [
     @board[index] = player
   end
 
+  def position_taken?(board, index)
+  if board[index] != ("X") && board[index] != ("O")
+    return false
+  elsif board[index] == ("X") || board[index] == ("O")
+    return true
+  end
+end
+
 end
